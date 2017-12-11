1fc8a726c52f79cc0de71816780692ca73535e07
Arun Koshi <arunkoshi@indix.com>
2017-12-11 14:50:27 +0530

    changes to docker publish

--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -2,5 +2,10 @@
 echo Publishing docker image.........
 echo $TRAVIS_BUILD_NUMBER
 echo $AKKA_CONTAINER_NAME
-docker build -t akka-sample:0.1 ../
+pwd
+ls -lrh
+cd ..
+pwd
+ls -lrh
+docker build -t akka-sample:0.1 .
 docker images
