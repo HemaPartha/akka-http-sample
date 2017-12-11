dc4ceb2672ddcd3960e0ef0faf12452ce0ece3a1
Arun Koshi <arunkoshi@indix.com>
2017-12-11 15:37:16 +0530

    Added docker commands

--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -2,5 +2,11 @@
 echo Publishing docker image.........
 echo $TRAVIS_BUILD_NUMBER
 echo $AKKA_CONTAINER_NAME
-docker build -t akka-sample:0.1 .
+echo $DOCKER_LOGIN
+
+docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
+docker build -t akka-http-sample .
 docker images
+docker tag akka-http-sample arunkoshi/samples:akka-http-sample-$TRAVIS_BUILD_NUMBER
+docker push arunkoshi/samples:akka-http-sample-$TRAVIS_BUILD_NUMBER
+
