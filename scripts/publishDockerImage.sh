63c279ad4b521f10eb2f596e5b0d838dc2e9f573
Arun Koshi <arunkoshi@indix.com>
2017-12-12 11:45:55 +0530

    Changed docker script to use more generic environment name

--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 echo Publishing docker image.........
 echo $TRAVIS_BUILD_NUMBER
-echo $AKKA_CONTAINER_NAME
-echo $DOCKER_LOGIN
+echo $BUILD_NUMBER
 
 docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
 docker build -t akka-http-sample .
 docker images
-docker tag akka-http-sample arunkoshi/samples:akka-http-sample-$TRAVIS_BUILD_NUMBER
-docker push arunkoshi/samples:akka-http-sample-$TRAVIS_BUILD_NUMBER
+docker tag akka-http-sample arunkoshi/samples:akka-http-sample-$BUILD_NUMBER
+docker push arunkoshi/samples:akka-http-sample-$BUILD_NUMBER
 
